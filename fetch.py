8b95dc7351249cfda7308ad1559826039c8647d6
Iridium <iridiummx@gmail.com>
2016-09-22 14:52:07 -0700

    new java branch

---
+++ fetch.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+#import json
+import requests
+import os
+#import re
+
+r = requests.get("https://leetcode.com/api/problems/algorithms/")
+r = r.json()
+for q in r['stat_status_pairs']:
+    title = q['stat']['question__title_slug']
+    if(not os.path.exists('TODO/' + title)): os.mkdir('TODO/' + title)
+    with open('TODO/' + title + '/README.md', 'w') as f:
+        f.write('''
+some damn template
+    ''')
+    open('TODO/' + title + '/Solution.java', 'w')
+    #url = "https://leetcode.com/problems/"+title
+    #content = requests.get(url).content
+    #re.findall("'value': 'java'")
