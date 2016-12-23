a44c6eafee9cfce22336639cdb2a71417cedac4b
Iridium <iridiummx@gmail.com>
2016-12-22 20:59:02 -0800

    3 fishers

--- fetch.py
+++ fetch.py
@@ -9,8 +9,11 @@ r = requests.get("https://leetcode.com/api/problems/algorithms/")
 r = r.json()
 for q in r['stat_status_pairs']:
     title = q['stat']['question__title_slug']
-    if(not os.path.exists('TODO/' + title)): os.mkdir('TODO/' + title)
-    open('TODO/' + title + '/Solution.java', 'w')
+    id = q['stat']['question_id']
+    dir = 'TODO/' + str(id) + '-' + title
+    if(not os.path.exists(dir)):
+        os.mkdir(dir)
+    open(dir + '/Solution.java', 'w')
     #url = "https://leetcode.com/problems/"+title
     #content = requests.get(url).content
     #re.findall("'value': 'java'")
